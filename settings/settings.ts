1c37553d50aa87c9e6cc6fafc70f9da4d97ff0ac
jakapozun <jaka.pozun2@gmail.com>
2025-01-06 16:17:28 +0100

    Iniital commit

---
+++ settings/settings.ts
@@ -0,0 +1,28 @@
+export const url = "https://docs.ixian.io/"
+export const siteicon = "/icon.png"
+
+export const gtm = "GTM-XXXXXXX"
+export const gtmconnected = true
+
+export const sitename = "ixidocs"
+export const description =
+  "Ixian documentation. Learn about Ixian and how to get started quickly."
+export const keywords = [
+  "documentation",
+  "Ixian",
+  "open source",
+]
+export const urlimage = "/images/og-image.png"
+export const imagealt = "Ixian Documentation"
+export const twitterhandle = "@ixian_io"
+
+export const companyname = "Ixian"
+export const companylink = "https://www.ixian.io/"
+
+export const branding = true
+export const rightsidebar = true
+export const feedbackedit = true
+export const tableofcontent = true
+export const totopscroll = true
+
+export const loadfromgithub = false
